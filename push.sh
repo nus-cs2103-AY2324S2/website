53f39f69413b8646a4c94948d65d20d853868519
damithc <damithc@github>
2024-02-16 19:12:26 +0800

    Site Update. [skip ci]

--- push.sh
+++
@@ -1,7 +0,0 @@
-set -x
-
-cd book
-git push origin master
-cd ..
-git push origin cs2103
-git status
--- pushall.sh
+++
@@ -1,8 +0,0 @@
-set -x
-cd book
-git push origin master
-cd ..
-git push upstream master
-git push origin master
-git push --force-with-lease origin cs2103
-git status
