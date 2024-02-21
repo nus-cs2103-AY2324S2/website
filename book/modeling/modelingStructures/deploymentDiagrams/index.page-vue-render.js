2fb6c3a2713fab82635503e2887c379d80e042b0
damithc <damithc@github>
2024-02-21 22:32:14 +0800

    Site Update. [skip ci]

--- book/modeling/modelingStructures/deploymentDiagrams/index.page-vue-render.js
+++ book/modeling/modelingStructures/deploymentDiagrams/index.page-vue-render.js
@@ -17,6 +17,6 @@ with(this){return _c('div',[_c('h4',{attrs:{"id":"deployment-diagrams"}},[_c('sp
 with(this){return _c('p',[_c('strong',[_v("A "),_c('em',[_v("deployment diagram")]),_v(" shows a system's physical layout, revealing which pieces of software run on which pieces of hardware.")])])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.2.0")]),_v(", generated on Wed, 21 Feb 2024, 0:30:25 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.2.0")]),_v(", generated on Wed, 21 Feb 2024, 22:27:47 GMT+8]")])])])}
 }];
   
