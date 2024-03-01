fab6bb527ab036d1c9c2ba6d167d4018c66dad75
damithc <damithc@github>
2024-03-02 00:57:26 +0800

    Site Update. [skip ci]

--- schedule/week1/index.page-vue-render.js
+++ schedule/week1/index.page-vue-render.js
@@ -41,6 +41,6 @@ with(this){return _c('li',[_c('span',[_v("[Optional] Follow the Git learning tra
 with(this){return _c('li',[_c('span',[_v("Attend the first weekly briefing")])])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.3.0")]),_v(", generated on Thu, 29 Feb 2024, 17:55:24 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.3.0")]),_v(", generated on Sat, 2 Mar 2024, 0:53:09 GMT+8]")])])])}
 }];
   
--- schedule/week7/admin.page-vue-render.js
+++ schedule/week7/admin.page-vue-render.js
@@ -14,6 +14,6 @@ with(this){return _c('h1',{attrs:{"id":"week-7-fri-feb-23rd-admin"}},[_v("Week 7
 with(this){return _c('div',[_c('ul',[_c('li',[_c('strong',[_v("Weekly quiz")]),_v(": Read weekly topics allocated for this week and submit the weekly quiz before the quiz deadline "),_c('span',{staticClass:"dimmed"},[_v("(i.e., before the following\nweekly briefing)")]),_v(".")])])])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.3.0")]),_v(", generated on Thu, 29 Feb 2024, 17:55:24 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.3.0")]),_v(", generated on Sat, 2 Mar 2024, 0:53:09 GMT+8]")])])])}
 }];
   
