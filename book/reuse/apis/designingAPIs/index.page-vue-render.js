332a2014ae278b868d51a5e9d7c5e9eafc3a50a6
damithc <damithc@github>
2024-04-06 14:11:51 +0800

    Site Update. [skip ci]

--- book/reuse/apis/designingAPIs/index.page-vue-render.js
+++ book/reuse/apis/designingAPIs/index.page-vue-render.js
@@ -17,6 +17,6 @@ with(this){return _c('div',[_c('h4',{attrs:{"id":"designing-apis"}},[_c('span',[
 with(this){return _c('p',[_v("You can use UML "),_c('em',[_v("sequence diagrams")]),_v(" to analyze the required interactions between components in order to discover the required API. Given below is an example.")])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.4.0")]),_v(", generated on Sat, 6 Apr 2024, 10:08:59 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.4.0")]),_v(", generated on Sat, 6 Apr 2024, 14:07:53 GMT+8]")])])])}
 }];
   
