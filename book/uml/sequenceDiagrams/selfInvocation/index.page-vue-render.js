589f2da01ee869c33b2741456a29b368d0b55cfe
damithc <damithc@github>
2024-04-07 20:51:55 +0800

    Site Update. [skip ci]

--- book/uml/sequenceDiagrams/selfInvocation/index.page-vue-render.js
+++ book/uml/sequenceDiagrams/selfInvocation/index.page-vue-render.js
@@ -23,6 +23,6 @@ with(this){return _c('p',[_c('span',{staticClass:"badge rounded-pill bg-success 
 with(this){return _c('code',{pre:true,attrs:{"class":"no-line-numbers hljs java"}},[_c('span',[_v("Book b = "),_c('span',{pre:true,attrs:{"class":"hljs-keyword"}},[_v("new")]),_v(" Book();\n")]),_c('span',[_v("Chapter c = "),_c('span',{pre:true,attrs:{"class":"hljs-keyword"}},[_v("new")]),_v(" Chapter();\n")]),_c('span',[_v("b.add(c);\n")])])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.4.0")]),_v(", generated on Sun, 7 Apr 2024, 18:41:31 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.4.0")]),_v(", generated on Sun, 7 Apr 2024, 20:47:39 GMT+8]")])])])}
 }];
   
